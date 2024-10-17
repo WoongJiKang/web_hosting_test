778bf472616523dcf38a0cc939963183903ffaa5
WoongJiKang <121491959+WoongJiKang@users.noreply.github.com>
2024-10-17 19:07:24 +0900

    Detach CSS, Change Design
    1) 간략화, 코드 가시성 위해 CSS 분리
    2) UI 가시성을 높이기 위한 디자인 및 색상 변경

--- source.js
+++ source.js
@@ -10,36 +10,35 @@ function showAmounts() {
     const divisor = 100000000; // 1억
 
     // 결과를 문자열로 만듭니다.
-    let resultText = `입력된 골드 주화 교환가: ${formatNumber(document.getElementById("goldAmount").value)}메소\n`;
-    resultText += `입력된 메소 판매가: ${formatNumber(document.getElementById("mesoAmount").value)}메포\n`;
+    let resultText = `입력된 <span style="color: #EDD53F;">골드 주화 교환가</span>: ${formatNumber(document.getElementById("goldAmount").value)}메소<br>`;
+    resultText += `입력된 <span style="color: #3d5a64;">메소마켓 메소 판매가</span>: ${formatNumber(document.getElementById("mesoAmount").value)}메포<br>`;
 
     // goldAmount가 0보다 클 경우 나누기 결과를 계산합니다.
     if (goldAmount > 0) {
         const adjustedGoldAmount = goldAmount / divisor; // 골드 주화를 1억으로 나눈 값
         const divisionResult = (comparisonValue / adjustedGoldAmount).toFixed(2); // 소수점 2자리까지 표시
-        const comparisonWithMeso = (mesoAmount / adjustedGoldAmount).toFixed(2); // 메소와의 비교 결과
 
-        resultText += `\n계산 결과: ${comparisonValue} ÷ ${mesoAmount} = ${divisionResult}\n`;
+        resultText += `<br>계산 결과: ${comparisonValue} ÷ ${adjustedGoldAmount} = ${divisionResult}<br>`;
         
         // 비교 결과를 계산하여 출력합니다.
         let comparisonResult;
         if (mesoAmount > adjustedGoldAmount) {
-            comparisonResult = "메소마켓 메소 판매가 > 골드 주화 교환가\n";
+            comparisonResult = "<span style='color: #3d5a64;'>메소마켓 메소 판매가</span> > <span style='color: #EDD53F;'>골드 주화 교환가</span><br>";
         } else if (mesoAmount < adjustedGoldAmount) {
-            comparisonResult = "메소마켓 메소 판매가 < 골드 주화 교환가\n";
+            comparisonResult = "<span style='color: #3d5a64;'>메소마켓 메소 판매가</span> < <span style='color: #EDD53F;'>골드 주화 교환가</span><br>";
         } else {
-            comparisonResult = "메소마켓 메소 판매가 = 골드 주화 교환가\n";
+            comparisonResult = "<span style='color: #3d5a64;'>메소마켓 메소 판매가</span> = <span style='color: #EDD53F;'>골드 주화 교환가</span><br>";
         }
 
         // 결과 문자열에 비교 결과와 추가 메시지를 포함합니다.
-        resultText += `\n비교 결과: ${comparisonResult}\n`;
-        resultText += `골드 주화 = ${formatNumber(adjustedGoldAmount.toString())}일 때, 메소 판매 비율은 ${divisionResult} 이상이어야 한다.`;
+        resultText += `비교 결과: ${comparisonResult}<br>`;
+        resultText += `골드 주화 = ${formatNumber(adjustedGoldAmount.toString())}일 때, 메소 판매 비율은 <span style='color: Red;'>${divisionResult}메포</span> 이상이어야 한다.`;
 
     } else {
-        resultText += `계산할 수 없습니다. 골드 주화 교환가가 0이어서는 안됩니다.`;
+        resultText += `계산할 수 없습니다. <span style="color: #EDD53F;">골드 주화 교환가</span> 혹은 <span style="color: #3d5a64;">메소마켓 메소 판매가</span>가 0 이거나 그 이하입니다.`;
     }
 
-    document.getElementById("result").innerText = resultText;
+    document.getElementById("result").innerHTML = resultText; // innerHTML 사용
 }
 
 // 입력 값이 바뀔 때마다 포맷팅 함수 호출
